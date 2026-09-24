23696e9eb7d3c4ada868a3cf4d07d7a15130d90d
aerubi <austin.berry2017@gmail.com>
2026-09-24 16:14:10 -0500

    Fixed Import

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import mock01 from '../assets/images/mock01.png';
-import mock06 from '../assets/images/mock06.png';
 import mock02 from '../assets/images/mock02.png';
+import mock03 from '../assets/images/mock06.png';
 import mock04 from '../assets/images/mock04.png';
+import mock06 from '../assets/images/mock06.png';
 import mock09 from '../assets/images/mock09.png';
 import '../assets/styles/Project.scss';
 
